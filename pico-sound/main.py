dd1a690c12c25e6affd111b2fc0d59db80445a79
Your Name <you@example.com>
2026-05-07 09:18:02 -0400

    random

--- pico-sound/main.py
+++ pico-sound/main.py
@@ -1,4 +1,5 @@
 from machine import Pin, PWM
+from random import randrange
 from time import sleep_ms
 
 BUTTON_PIN = 14
@@ -8,6 +9,10 @@ SPEAKER_PIN = 16
 NOTE_FREQUENCY_HZ = 440
 NOTE_DURATION_MS = 1000
 NOTE_DUTY = 20000
+RANDOM_NOTE_FREQUENCIES_HZ = [262, 294, 330, 349, 392, 440, 494, 523]
+RANDOM_SEQUENCE_LENGTH = 8
+RANDOM_NOTE_DURATION_MS = 180
+RANDOM_NOTE_GAP_MS = 70
 
 BUTTON_DEBOUNCE_MS = 20
 BUTTON_POLL_MS = 10
@@ -30,7 +35,15 @@ def play_note(frequency_hz=NOTE_FREQUENCY_HZ, duration_ms=NOTE_DURATION_MS):
     status_led.off()
 
 
-print("Press the button on GP14 to play a note on the speaker driver connected to GP16.")
+def play_random_sequence(note_count=RANDOM_SEQUENCE_LENGTH):
+    for _ in range(note_count):
+        note_index = randrange(len(RANDOM_NOTE_FREQUENCIES_HZ))
+        frequency_hz = RANDOM_NOTE_FREQUENCIES_HZ[note_index]
+        play_note(frequency_hz, RANDOM_NOTE_DURATION_MS)
+        sleep_ms(RANDOM_NOTE_GAP_MS)
+
+
+print("Press the button on GP14 to play random notes on the speaker driver connected to GP16.")
 print("Status LED on GP15 turns on while the note is playing.")
 speaker.duty_u16(0)
 status_led.off()
@@ -44,7 +57,7 @@ try:
             sleep_ms(BUTTON_DEBOUNCE_MS)
 
             if button_is_pressed():
-                play_note()
+                play_random_sequence()
 
                 while button_is_pressed():
                     sleep_ms(BUTTON_POLL_MS)
